0a74b23bacef49aed583ac990369b2d9a48de109
parkgyuro <qkrrbfh12@naver.com>
2021-09-10 14:19:21 +0900

    finish web version

--- js/main.js
+++ js/main.js
@@ -1,7 +1,106 @@
+const subMenuBtn = document.querySelectorAll('.nav-menu');
+const subMenuBtns = document.querySelector('.nav-container');
+const subMenuContainer = document.querySelector('.header-sub-menu-container')
+const modal = document.querySelector('.modal');
+const observeReady = document.querySelectorAll('.observe-ready');
+const firstSectionImgContainer = document.querySelector('.first-section-img-container');
+const shoesItemContainer = document.querySelector('.shoes-items');
+
+const documentHeight = document.body.scrollHeight;
+const ACTIVE = 'active';
+
+let firstSectionSpeed = 1;
+let shoesContainerSpeed = 1;
+
+function showContent(){
+    const io = new IntersectionObserver((item, observe) => {
+        for(let i=0; i<item.length; i++){
+            if(item[i].isIntersecting){
+                item[i].target.classList.add(ACTIVE);
+            }
+            else{
+                item[i].target.classList.remove(ACTIVE);
+            }
+        }
+    });
+    for(let i=0; i<observeReady.length; i++){
+        io.observe(observeReady[i]);
+    }
+}
+
+function setSubMenuIndex(){
+    for(let i=0; i<subMenuBtn.length; i++){
+        subMenuBtn[i].dataset.index = i;
+    }
+}
+
 function loadingPageRemove(){
-    document.body.classList.add('active');
+    document.body.classList.add(ACTIVE);
+}
+
+function setModal(){
+    modal.style.height = `${documentHeight}px`;
+}
+
+function showSubMenu(targetIndex){
+    subMenuContainer.innerHTML = subMenuData[targetIndex];
+    setModal();
+    modal.classList.add(ACTIVE);
+    subMenuContainer.classList.add(ACTIVE);
+
+    document.body.addEventListener('mouseover', (e) => {
+        let target = e.target;
+
+        if(target === modal){
+            modal.classList.remove(ACTIVE);
+            subMenuContainer.classList.remove(ACTIVE);
+        }
+    })
+}
+
+function contentAutoScroll(){
+    const scrollAmount = firstSectionImgContainer.scrollWidth - firstSectionImgContainer.clientWidth
+    const shoesItemScrollAmount = shoesItemContainer.scrollWidth - shoesItemContainer.clientWidth;
+
+    firstSectionImgContainer.scrollLeft += firstSectionSpeed;
+    shoesItemContainer.scrollLeft += shoesContainerSpeed;
+
+    if(firstSectionImgContainer.scrollLeft === scrollAmount){
+        firstSectionSpeed *= -1;
+    }
+    else if(firstSectionImgContainer.scrollLeft === 0){
+        firstSectionSpeed *= -1;
+    }
+
+    if(shoesItemContainer.scrollLeft === shoesItemScrollAmount){
+        shoesContainerSpeed *= -1;
+    }
+    else if(shoesItemContainer.scrollLeft === 0){
+        shoesContainerSpeed *= -1;
+    }
+
+    window.requestAnimationFrame(contentAutoScroll);
 }
 
 window.addEventListener('load', () => {
+    setSubMenuIndex();
     loadingPageRemove();
-})
+    showContent();
+    contentAutoScroll();
+});
+
+subMenuBtns.addEventListener('mouseover', (e) => {
+    let target = e.target;
+    if(target === subMenuBtns){
+        return;
+    }
+    while(!target.dataset.index){
+        target = target.parentNode;
+    }
+
+    let targetIndex = parseInt(target.dataset.index);
+    showSubMenu(targetIndex);
+});
+
+
+
---
+++ js/sub-menu-data.js
@@ -0,0 +1,345 @@
+const subMenuData = [
+    `
+            <ul class="header-sub-menu">New&Featured
+                <li>
+                    <a href="#">New Arrived</a>
+                </li>
+                <li>
+                    <a href="#">Air Force 01</a>
+                </li>
+                <li>
+                    <a href="#">Waffle Trainer</a>
+                </li>
+                <li>
+                    <a href="#">ACG</a>
+                </li>
+                <li>
+                    <a href="#">Nike Lab</a>
+                </li>
+                <li>
+                    <a href="#">New To Sale</a>
+                </li>
+            </ul>
+            <ul class="header-sub-menu">New For Men
+                <li>
+                    <a href="#">Shoes</a>
+                </li>
+                <li>
+                    <a href="#">Cloth</a>
+                </li>
+                <li>
+                    <a href="#">Etc</a>
+                </li>
+                <li>
+                    <a href="#">More</a>
+                </li>
+            </ul>
+            <ul class="header-sub-menu">New For Men
+                <li>
+                    <a href="#">Shoes</a>
+                </li>
+                <li>
+                    <a href="#">Cloth</a>
+                </li>
+                <li>
+                    <a href="#">Etc</a>
+                </li>
+                <li>
+                    <a href="#">More</a>
+                </li>
+            </ul>
+            <ul class="header-sub-menu">New For kids
+                <li>
+                    <a href="#">Shoes</a>
+                </li>
+                <li>
+                    <a href="#">Cloth</a>
+                </li>
+                <li>
+                    <a href="#">Etc</a>
+                </li>
+                <li>
+                    <a href="#">More</a>
+                </li>
+            </ul>
+    `,
+    `
+        <ul class="header-sub-menu">New&Featured
+            <li>
+                <a href="#">New Arrived<a>
+            </li>
+            <li>
+                <a href="#">Air Force 01<a>
+            </li>
+            <li>
+                <a href="#">Waffle Trainer<a>
+            </li>
+            <li>
+                <a href="#">ACG<a>
+            </li>
+            <li>
+                <a href="#">Nike Lab<a>
+            </li>
+            <li>
+                <a href="#">New To Sale<a>
+            </li>
+        </ul>
+        <ul class="header-sub-menu">Shoes
+            <li>
+                <a href="#">More<a>
+            </li>
+            <li>
+                <a href="#">Life Style<a>
+            </li>
+            <li>
+                <a href="#">Running<a>
+            </li>
+            <li>
+                <a href="#">Training<a>
+            </li>
+            <li>
+                <a href="#">Basketball<a>
+            </li>
+            <li>
+                <a href="#">Jordan<a>
+            </li>
+            <li>
+                <a href="#">Football<a>
+            </li>
+            <li>
+                <a href="#">Golf<a>
+            </li>
+        </ul>
+        <ul class="header-sub-menu">Cloth
+            <li>
+                <a href="#">More<a>
+            </li>
+            <li>
+                <a href="#">Hoodie<a>
+            </li>
+            <li>
+                <a href="#">Pants<a>
+            </li>
+            <li>
+                <a href="#">Jecket<a>
+            </li>
+            <li>
+                <a href="#">T-shirts<a>
+            </li>
+        </ul>
+        <ul class="header-sub-menu">Sport
+            <li>
+                <a href="#">Running<a>
+            </li>
+            <li>
+                <a href="#">Training<a>
+            </li>
+            <li>
+                <a href="#">Football<a>
+            </li>
+            <li>
+                <a href="#">Basketball<a>
+            </li>
+            <li>
+                <a href="#">Golf<a>
+            </li>
+        </ul>
+`,
+    `
+    <ul class="header-sub-menu">New&Featured
+            <li>
+                <a href="#">New Arrived<a>
+            </li>
+            <li>
+                <a href="#">Air Force 01<a>
+            </li>
+            <li>
+                <a href="#">Waffle Trainer<a>
+            </li>
+            <li>
+                <a href="#">ACG<a>
+            </li>
+            <li>
+                <a href="#">Nike Lab<a>
+            </li>
+            <li>
+                <a href="#">New To Sale<a>
+            </li>
+        </ul>
+        <ul class="header-sub-menu">Shoes
+            <li>
+                <a href="#">More<a>
+            </li>
+            <li>
+                <a href="#">Life Style<a>
+            </li>
+            <li>
+                <a href="#">Running<a>
+            </li>
+            <li>
+                <a href="#">Training<a>
+            </li>
+            <li>
+                <a href="#">Basketball<a>
+            </li>
+            <li>
+                <a href="#">Jordan<a>
+            </li>
+            <li>
+                <a href="#">Football<a>
+            </li>
+            <li>
+                <a href="#">Golf<a>
+            </li>
+        </ul>
+        <ul class="header-sub-menu">Cloth
+            <li>
+                <a href="#">More<a>
+            </li>
+            <li>
+                <a href="#">Hoodie<a>
+            </li>
+            <li>
+                <a href="#">Pants<a>
+            </li>
+            <li>
+                <a href="#">Jecket<a>
+            </li>
+            <li>
+                <a href="#">T-shirts<a>
+            </li>
+        </ul>
+        <ul class="header-sub-menu">Sport
+            <li>
+                <a href="#">Running<a>
+            </li>
+            <li>
+                <a href="#">Training<a>
+            </li>
+            <li>
+                <a href="#">Football<a>
+            </li>
+            <li>
+                <a href="#">Basketball<a>
+            </li>
+            <li>
+                <a href="#">Golf<a>
+            </li>
+        </ul>
+`
+,
+    `
+    <ul class="header-sub-menu">New&Featured
+            <li>
+                <a href="#">New Arrived</a>
+            </li>
+            <li>
+                <a href="#">Air Force 01</a>
+            </li>
+            <li>
+                <a href="#">Waffle Trainer</a>
+            </li>
+            <li>
+                <a href="#">ACG</a>
+            </li>
+            <li>
+                <a href="#">Nike Lab</a>
+            </li>
+            <li>
+                <a href="#">New To Sale</a>
+            </li>
+        </ul>
+        <ul class="header-sub-menu">Shoes
+            <li>
+                <a href="#">More</a>
+            </li>
+            <li>
+                <a href="#">Baby(160mm-)</a>
+            </li>
+            <li>
+                <a href="#">Little Kids(180mm-220mm)</a>
+            </li>
+            <li>
+                <a href="#">Junior(225mm-250mm)</a>
+            </li>
+            </ul>
+        <ul class="header-sub-menu">Cloth
+            <li>
+                <a href="#">More</a>
+            </li>
+            <li>
+                <a href="#">Baby(0-4age)</a>
+            </li>
+            <li>
+                <a href="#">Little Kids(4-7age)</a>
+            </li>
+            <li>
+                <a href="#">Junior(8-13age)</a>
+            </li>
+        </ul>
+        <ul class="header-sub-menu">Sport
+            <li>
+                <a href="#">Running</a>
+            </li>
+            <li>
+                <a href="#">Training</a>
+            </li>
+            <li>
+                <a href="#">Football</a>
+            </li>
+            <li>
+                <a href="#">Basketball</a>
+            </li>
+            <li>
+                <a href="#">Tennis</a>
+            </li>
+        </ul>
+`
+,
+    `
+    <ul class="header-sub-menu">New To Sale
+    </ul>
+    <ul class="header-sub-menu">Mens Sale
+        <li>
+            <a href="#">Shoes</a>
+        </li>
+        <li>
+            <a href="#">Cloth</a>
+        </li>
+        <li>
+            <a href="#">Etc</a>
+        </li>
+        <li>
+            <a href="#">More</a>
+        </li>
+    </ul>
+    <ul class="header-sub-menu">Womens Sale
+        <li>
+            <a href="#">Shoes</a>
+        </li>
+        <li>
+            <a href="#">Cloth</a>
+        </li>
+        <li>
+            <a href="#">Etc</a>
+        </li>
+        <li>
+            <a href="#">More</a>
+        </li>
+    </ul>
+    <ul class="header-sub-menu">Kids Sale
+        <li>
+            <a href="#">Shoes</a>
+        </li>
+        <li>
+            <a href="#">Cloth</a>
+        </li>
+        <li>
+            <a href="#">Etc</a>
+        </li>
+        <li>
+            <a href="#">More</a>
+        </li>
+    </ul>
+`
+]
